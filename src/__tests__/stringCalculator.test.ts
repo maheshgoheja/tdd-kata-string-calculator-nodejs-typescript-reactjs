4fa9542a77975d5c6fd8ffaa0bd734b85eb9e2db
Mahesh Goheja <msgoheja@gmail.com>
2025-02-13 19:09:57 +0530

    feat: Implement Step 2 - Single number returns itself

--- src/__tests__/stringCalculator.test.ts
+++ src/__tests__/stringCalculator.test.ts
@@ -5,4 +5,10 @@ describe("String Calculator", () => {
     const Calculator = new StringCalculator();
     expect(Calculator.add("")).toBe(0);
   });
+
+  it("should return the number itself if it is a single number", () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add("1")).toBe(1);
+    expect(calculator.add("5")).toBe(5);
+  });
 });
